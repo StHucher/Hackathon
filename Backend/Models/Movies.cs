b84e219e75c65c97c95127c1a75fbd7ad9e6ffd4
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-15 14:34:47 +0100

    Link with Db

---
+++ Backend/Models/Movies.cs
@@ -0,0 +1,9 @@
+
+public class Movies
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public  required string Autor { get; set; }
+    public string? Genre { get; set; }
+    
+}
